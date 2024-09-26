ba2ec4dae3b842bcb34f3cf6f07b9e96ee82c885
Madan-ghodechor <madanghodechor6232@gmail.com>
2024-09-26 21:50:16 +0530

    file added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('../routes/apiRoutes');
 const app = express();
 
 app.use(express.json());
